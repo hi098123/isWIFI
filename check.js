c402e23c2295222ada05acf6424346b45ec81c61
hi098123 <46213700+hi098123@users.noreply.github.com>
2020-08-04 06:21:28 +0900

    Create check.js

---
+++ check.js
@@ -0,0 +1,32 @@
+/*
+  isWIFI()
+  {
+    -1: undefined or can't find
+    0: cellular(mobile 3g, 4g, 5g, etc.)
+    1: saveData=true (cellular or wifi or PC)	saveData is true (browser setting)
+    2: saveData=false (cellular or wifi or PC)
+    3: wifi
+  }
+*/
+function isWIFI() {
+  var conn = navigator.connection || navigator.mozConnection || navigator.webkitConnection || 0;
+  if(!conn){return -1;}
+  if(typeof(conn.type)!="undefined"){
+    switch(conn.type){
+      case "wifi":
+        return 3;
+      case "cellular":
+        return 0;
+      default:
+        return -1;
+    }
+  }else if(typeof(conn.saveData)!="undefined"){
+    if(conn.saveData){
+      return 1;
+    }else{
+      return 2;
+    }
+  }else{
+    return -1;
+  }
+}
